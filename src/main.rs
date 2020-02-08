ab8036c5c2ee67520405ffe7980b3da2c2f23cd4
Ntacman <Ntacman@users.noreply.github.com>
2020-02-08 01:42:22 -0600

    Refactor to use tera templating engine

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,22 @@
 #[macro_use] extern crate rocket_contrib;
 #[macro_use] extern crate serde_derive;
 #[macro_use] extern crate lazy_static;
+#[macro_use] extern crate tera;
 
 use std::env;
 use rocket_contrib::json::{Json, JsonValue};
 use rocket_contrib::serve::StaticFiles;
+use rocket_contrib::templates::Template;
+use rocket::response::Redirect;
+use rocket::Request;
 use std::collections::VecDeque;
 use std::sync::{Mutex, Arc};
-
+use tera::Context;
 
 #[derive(Serialize, Deserialize)]
 struct PlaylistItem {
   url: String,
-  name: String,
-  index: u16,
+  name: String
 }
 
 lazy_static! {
@@ -30,10 +33,17 @@ fn playlist_info() -> JsonValue {
   )
 }
 
+#[get("/")]
+fn index() -> Template {
+  let song = PLAYLISTMUTEX.clone().lock().unwrap().pop_front();
+  let context = song.unwrap_or(PlaylistItem{url: "empty".to_owned(), name: "empty".to_owned()});
+  Template::render("index", &context)
+}
+
 #[put("/add", data = "<item>")]
 fn add_song(item: Json<PlaylistItem>) -> JsonValue {
   PLAYLISTMUTEX.clone().lock().unwrap().push_back(item.0); 
-  json!({"status": "ok"})
+  json!({"status": "ok", "items": *PLAYLISTMUTEX.clone().lock().unwrap()})
 }
 
 fn main() {
@@ -42,17 +52,18 @@ fn main() {
   println!("Static File Path is : {}/static", path);
 
   unsafe{
-    PLAYLISTMUTEX.clone().lock().unwrap().push_back(PlaylistItem{url: "test".to_owned(), name: "test".to_owned(), index: 0});
-    PLAYLISTMUTEX.clone().lock().unwrap().push_back(PlaylistItem{url: "test1".to_owned(), name: "test1".to_owned(), index: 1});
-    PLAYLISTMUTEX.clone().lock().unwrap().push_back(PlaylistItem{url: "test2".to_owned(), name: "test2".to_owned(), index: 2});
+    PLAYLISTMUTEX.clone().lock().unwrap().push_back(PlaylistItem{url: "test".to_owned(), name: "test".to_owned()});
+    PLAYLISTMUTEX.clone().lock().unwrap().push_back(PlaylistItem{url: "test1".to_owned(), name: "test1".to_owned()});
+    PLAYLISTMUTEX.clone().lock().unwrap().push_back(PlaylistItem{url: "test2".to_owned(), name: "test2".to_owned()});
   }
   
   for x in PLAYLISTMUTEX.clone().lock().unwrap().iter() {
-    println!("{}{}{}", x.url, x.name, x.index);
+    println!("{}{}", x.url, x.name);
   }
   
   rocket::ignite()
     .mount("/api/", routes![playlist_info, add_song])
-    .mount("/", StaticFiles::from(format!("{}{}", path, "/static")))
+    .mount("/", routes![index])
+    .attach(Template::fairing())
     .launch();
 }
