b06503b9d56b727c30199c0f5abf668d15f2984b
Ntacman <Ntacman@users.noreply.github.com>
2020-02-16 22:23:21 -0600

    debug

--- static/main.js
+++ static/main.js
@@ -2,28 +2,50 @@ document.addEventListener('DOMContentLoaded', function() {
   var player = document.getElementById('player');
   console.log(player);
   player.addEventListener("ended", function(e){
-    var request = new XMLHttpRequest()
-    var base = location.origin;
-    
-    request.open('GET', base + '/api/get_next_song', true)
-    
-    request.onload = function() {
-      var data = JSON.parse(this.response)
-      if (request.status >= 200 && request.status < 400) {
-        document.getElementById('now-playing').innerHTML = ("Now Playing: " + data.name);
-        set_src(data.url, data.name, player);
-      } else {
-        console.log('error');
-      }
+    var data = get_next_song(player);
+    if (data == null) {
+      set_src("empty", "empty", player);
+    } else {
+      set_src(data.url, data.name, player);
     }
-  
-    request.send();
   }, false);
+
+  setInterval(function(){
+    check_if_empty(player); 
+  }, 1000);
 })
 
 function set_src(url, title, player) {
   console.log('Next song playing is: ' + title);
+  document.getElementById('now-playing').innerHTML = ("Now Playing: " + title);
   player.setAttribute('src', url);
   player.load();
   player.play();
+}
+
+function check_if_empty(player) {
+  console.log(player.currentSrc.replace(location.origin, "") == '/empty');
+  if (player.currentSrc.replace(location.origin, "") == '/empty') {
+    var data = get_next_song(player);
+    if (data == null) {
+      set_src("empty", "empty", player);
+      return
+    } else {
+      set_src(data.url, data.name, player);
+    }
+  } else {
+    return
+  }
+}
+
+function get_next_song(player) {
+  var request = new XMLHttpRequest();
+  request.open ('GET', location.origin + '/api/get_next_song', false);
+  request.send();
+  if (request.status === 200) {
+    console.log(JSON.parse(request.response));
+    return JSON.parse(request.response);
+  } else {
+    console.log('Response failed');
+  }
 }
